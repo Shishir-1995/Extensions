312a144e2c35085af5ccd7d289258c1c4b7efade
Shishir-1995 <padhanshishir@gmail.com>
2022-12-12 10:05:02 +0530

    update : notification number added

--- Notification_Popup/scripts/content.js
+++ Notification_Popup/scripts/content.js
@@ -1,26 +1,40 @@
-const newNotification = document.querySelector(".max-w-7x1");
+console.log("Is it working ?");
 
-console.log(newNotification);
-
+let checkTime = false;
 function checkNotification() {
-	// window.location.reload();
+	const newNotificationBanner = document.querySelector(".ml-3 > span");
 
-	if (newNotification) {
-		Notification.requestPermission().then(() => {
-			var notification = new Notification("Masai Notification", {
-				icon: "https://www.masaischool.com/img/navbar/logo.svg",
-				body: "New Notification raise in LMS",
+	if (newNotificationBanner) {
+		let newMessage = newNotificationBanner.innerText.split(" ");
+
+		let numOfMessage = newMessage.pop();
+
+		const unreadMessage = newMessage.join(" ");
+
+		if (
+			unreadMessage == "You have unread notifications" &&
+			checkTime == false
+		) {
+			checkTime = true;
+
+			Notification.requestPermission().then(() => {
+				newMessage.push(numOfMessage);
+				var notification = new Notification("LMS Notification", {
+					icon: "https://www.masaischool.com/img/navbar/logo.svg",
+					body: newMessage.join(" "),
+				});
+				notification.onclick = function () {
+					clearInterval(intervalCheck);
+					checkTime = false;
+					window.open("https://course.masaischool.com/");
+				};
 			});
-			notification.onclick = function () {
-				window.open("https://course.masaischool.com/");
-			};
-		});
-
-		const title = document.querySelector("title");
-		title.innerText = "(1)*";
+		} else {
+			console.log("newNotification");
+		}
 	} else {
-		console.log("Notification  check is running...");
+		console.log("check is running");
 	}
 }
 
-setInterval(checkNotification, 3000);
+let intervalCheck = setInterval(checkNotification, 3000);
